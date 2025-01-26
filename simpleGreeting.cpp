0eb73f8e04835123bbca0e9d27192d344e87cc7c
bframirezvargas1 <BFRamirezVargas1@MY.SCCCD.EDU>
2025-01-26 13:18:59 -0800

    Add files via upload

---
+++ simpleGreeting.cpp
@@ -0,0 +1,24 @@
+// simpleGreeting.cpp
+// 01/26/2025
+// Brenton Ramirez-Vargas
+// Demo for CIT-66
+
+#include <iostream>
+
+int main() {
+    std::cout << " " << std::endl;
+
+    std::cout << "Hello! My name is Brenton Vargas." <<std::endl;
+    std::cout << "I am very excited to begin this coding class as it will greatly" <<std::endl;
+    std::cout << "prepare me for my career as an engineer. Although I am unfamiliar" <<std::endl;
+    std::cout << "with any coding language, I do strive to learn and give my best effort." << std::endl;
+
+    std::cout << " " << std::endl;
+    std::cout << "Kindest regards," << std::endl;
+
+
+    std::cout << " " <<std::endl;
+    std::cout << "Brenton Vargas " << std::endl;
+
+    return 0;
+}
