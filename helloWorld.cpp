e971338aae52384764f5c52a644ab51e41c332fe
bframirezvargas1 <BFRamirezVargas1@MY.SCCCD.EDU>
2025-01-26 13:19:14 -0800

    Add files via upload

---
+++ helloWorld.cpp
@@ -0,0 +1,14 @@
+// helloWorld.cpp
+// dH 01/20/2025
+// Brenton Ramirez Vargas
+// demo for CIT-66
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
