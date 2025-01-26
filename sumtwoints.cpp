79cc6bc453d97fb33ca41feeeb5c5f29222a7888
bframirezvargas1 <BFRamirezVargas1@MY.SCCCD.EDU>
2025-01-26 13:18:38 -0800

    Add files via upload

---
+++ sumtwoints.cpp
@@ -0,0 +1,20 @@
+// sumTwoInts.cpp
+// 01/25/2025
+// Brenton Ramirez Vargas
+//demo for CIT-66
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int num1 = 25;
+  int num2= 75;
+  int sum = num1 + num2;
+
+  std::cout << "First Integer: " << num1 << endl;
+  std ::cout << "Second Integer: " << num2 << endl;
+  std ::cout << "Sum of Integers: " << sum << endl;
+
+  return 0;
+ }
