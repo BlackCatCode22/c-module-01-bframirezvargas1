e909364d9184adb7bef7a035a017fd9fc3c35ab2
bframirezvargas1 <BFRamirezVargas1@MY.SCCCD.EDU>
2025-01-26 09:29:56 -0800

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,13 @@
+// helloWorld.cpp
+// dh 01/19/2025
+// demo for CIT-66
+/*
+ *Below will be
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!";
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,29 @@
+// helloWorld.cpp
+// dH 1/23/2025
+// Brenton Ramirez-Vargas
+// demo for CIT-66
+/*
+ * First off, I would like to say how cool this is!
+ * The computer will not read my comments.
+ * I love this feature!!!
+*/
+
+#include <iostream>
+using namespace std;
+
+int sumTwoints(int num1, int num2) {
+    return num1 + num2;
+}
+
+int main() {
+    //The statement will print "Hello World"
+
+    cout << "\n\n Hello, World! \n\n" << endl;
+
+    cout << sumTwoints(2, 3) << endl;
+
+    cout << "\n\n\n";
+
+
+    return 0;
+}
