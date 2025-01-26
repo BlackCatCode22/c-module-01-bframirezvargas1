0cf52c5407c573d16234e8c42921e954f003667b
bframirezvargas1 <BFRamirezVargas1@MY.SCCCD.EDU>
2025-01-26 13:19:29 -0800

    Add files via upload

---
+++ doubleInt.cpp
@@ -0,0 +1,18 @@
+// doubleInt.cpp
+// dH 01/21/2025
+// Brenton Ramirez Vargas
+// demo for CIT-66
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 25;
+    int doubleNum = num * 2;
+
+    std::cout << "Original Integer:" <<num << std::endl;
+    std::cout << "Double that number is:" <<doubleNum << std::endl;
+
+    return 0;
+}
